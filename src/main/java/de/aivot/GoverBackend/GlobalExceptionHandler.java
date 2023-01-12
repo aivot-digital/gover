42d25f9f8a3ad110a4178d658731014ba3efc208
Finn Moritz Bätzel <code@mobaetzel.de>
2023-01-12 13:51:12 +0100

    feat: Add global exception handler to send mail with stacktrace to report mail

---
+++ src/main/java/de/aivot/GoverBackend/GlobalExceptionHandler.java
@@ -0,0 +1,28 @@
+package de.aivot.GoverBackend;
+
+import de.aivot.GoverBackend.services.SystemMailService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.rest.webmvc.ResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+    private final SystemMailService systemMailService;
+
+    @Autowired
+    public GlobalExceptionHandler(SystemMailService systemMailService) {
+        this.systemMailService = systemMailService;
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<Object> exception(Exception ex, WebRequest request) throws Exception {
+        if (!(ex instanceof ResourceNotFoundException)) {
+            systemMailService.sendExceptionMail(ex);
+        }
+        return super.handleException(ex, request);
+    }
+}
