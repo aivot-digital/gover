f2c3bf4b054a58375d28146859ff728c2e0cd03e
Finn Moritz Bätzel <code@mobaetzel.de>
2023-05-07 18:13:27 +0200

    feat: Add smtp check function to app

---
+++ app/src/pages/staff-pages/settings/components/smtp-test/smtp-test.tsx
@@ -0,0 +1,135 @@
+import React, {FormEvent, useState} from 'react';
+import {Alert, AlertTitle, Box, Button, TextField, Typography} from '@mui/material';
+import axios from "axios";
+import {ApiConfig} from "../../../../../api-config";
+import {CrudService} from "../../../../../services/crud.service";
+
+const smtpTestAddress = `${ApiConfig.address}/system/test-smtp`;
+
+export function SmtpTest() {
+    const [targetEmail, setTargetEmail] = useState('');
+    const [isSending, setIsSending] = useState(false);
+    const [emailTestResult, setEmailTestResult] = useState<true | string>();
+
+    const handleSubmit = (event: FormEvent) => {
+        event.preventDefault();
+        event.stopPropagation();
+
+        const testPayload = {
+            targetMail: targetEmail,
+        };
+
+        setIsSending(true);
+        setEmailTestResult(undefined);
+
+        axios.post(
+            smtpTestAddress,
+            testPayload,
+            CrudService.getConfig()
+        )
+            .then(res => {
+                if (res.data().result != null) {
+                    setEmailTestResult(res.data().result);
+                } else {
+                    setEmailTestResult(true);
+                }
+            })
+            .catch(err => {
+                setEmailTestResult(err.message);
+            })
+            .finally(() => {
+                setIsSending(false);
+                setTargetEmail('');
+            });
+    };
+
+    return (
+        <>
+            <Typography variant="subtitle1">
+                E-Mail-Versand
+            </Typography>
+            <Typography>
+                Hier können Sie den E-Mail-Versand testen.
+                Geben Sie dazu eine Zieladresse ein, und klicken Sie auf <strong>E-Mail-Versand Testen</strong>.
+                Gover versendet eine Test-E-Mail an die angegebene Adresse.
+                Sollten Probleme auftreten, werden diese hier angezeigt.
+            </Typography>
+
+            {
+                emailTestResult != null &&
+                emailTestResult === true &&
+                <Alert
+                    severity="success"
+                    sx={{mt: 2}}
+                >
+                    <AlertTitle>
+                        Test erfolgreich!
+                    </AlertTitle>
+
+                    Der Test des E-Mail-Versands war erfolgreich.
+                    Sie sollten gleich eine E-Mail in Ihrem Postfach vorfinden.
+                </Alert>
+            }
+
+            {
+                emailTestResult != null &&
+                emailTestResult !== true &&
+                <Alert
+                    severity="error"
+                    sx={{mt: 2}}
+                >
+                    <AlertTitle>
+                        Versand fehlgeschlagen!
+                    </AlertTitle>
+
+                    <Typography>
+                        Beim Versand der Test-E-Mail ist ein Fehler aufgetreten.
+                        Mehr Informationen entnehmen Sie dem Fehlerbericht.
+                    </Typography>
+
+                    <Typography
+                        variant="subtitle2"
+                        sx={{mt: 2}}
+                    >
+                        Fehlerbericht
+                    </Typography>
+                    <Typography component="code">
+                        {emailTestResult}
+                    </Typography>
+                </Alert>
+            }
+
+            <form onSubmit={handleSubmit}>
+                <Box
+                    sx={{
+                        display: 'flex',
+                        mt: 2,
+                        alignItems: 'center',
+                    }}
+                >
+                    <TextField
+                        label="Zieladresse"
+                        type="email"
+                        placeholder="max.muster@mail.de"
+                        value={targetEmail}
+                        onChange={event => setTargetEmail(event.target.value)}
+                        onBlur={() => setTargetEmail(targetEmail.trim())}
+                        disabled={isSending}
+                        required
+                    />
+
+                    <Button
+                        type="submit"
+                        sx={{
+                            ml: 2,
+                            flexShrink: 0,
+                        }}
+                        disabled={isSending}
+                    >
+                        E-Mail-Versand Testen
+                    </Button>
+                </Box>
+            </form>
+        </>
+    );
+}
--- app/src/pages/staff-pages/settings/settings.tsx
+++ app/src/pages/staff-pages/settings/settings.tsx
@@ -13,6 +13,7 @@ import {Localization} from '../../../locale/localization';
 import {SystemInformation} from './components/system-information/system-information';
 import {useRoleGuard} from '../../../hooks/use-role-guard';
 import {UserRole} from '../../../data/user-role';
+import {SmtpTest} from "./components/smtp-test/smtp-test";
 
 const __ = Localization(strings);
 
@@ -51,8 +52,12 @@ export function Settings() {
                             value={2}
                         />
                         <Tab
-                            label={__.systemInfoTabTitle}
+                            label={__.smtpTestTitle}
                             value={3}
+                        />
+                        <Tab
+                            label={__.systemInfoTabTitle}
+                            value={4}
                         />
                     </Tabs>
                 </Box>
@@ -72,6 +77,10 @@ export function Settings() {
                     }
                     {
                         currentTab === 3 &&
+                        <SmtpTest/>
+                    }
+                    {
+                        currentTab === 4 &&
                         <SystemInformation/>
                     }
                 </Box>
