2793b3b57370c7c2e172ce0ceeadeb193c241ed4
Finn Moritz Bätzel <code@mobaetzel.de>
2023-05-27 18:00:42 +0200

    fix: Fix multi checkbox min required value

--- app/src/components/multi-checkbox-field/multi-checkbox-field.component.editor.tsx
+++ app/src/components/multi-checkbox-field/multi-checkbox-field.component.editor.tsx
@@ -1,7 +1,7 @@
-import {FormControl, TextField} from '@mui/material';
 import {MultiCheckboxFieldElement} from '../../models/elements/form/input/multi-checkbox-field-element';
 import {StringListInput} from "../string-list-input/string-list-input";
 import {BaseEditorProps} from "../../editors/base-editor";
+import {NumberFieldComponent} from "../number-field/number-field-component";
 
 export function MultiCheckboxFieldComponentEditor(props: BaseEditorProps<MultiCheckboxFieldElement>) {
     const minRequiredError = (
@@ -26,36 +26,16 @@ export function MultiCheckboxFieldComponentEditor(props: BaseEditorProps<MultiCh
 
             {
                 props.element.required &&
-                <FormControl>
-                    <TextField
-                        value={props.element.minimumRequiredOptions?.toString() ?? ''}
-                        label="Erforderliche Mindestanzahl"
-                        helperText={minRequiredError ? 'Sie fordern mehr Optionen als Sie angegeben haben.' : 'Geben Sie 0 ein, um keine Mindestanzahl zu fordern.'}
-                        onChange={event => {
-                            if (event.target.value === '') {
-                                props.onPatch({
-                                    minimumRequiredOptions: undefined,
-                                });
-                                return;
-                            }
-                            let val = parseInt(event.target.value ?? '0');
-                            if (isNaN(val)) {
-                                val = 0;
-                            }
-                            props.onPatch({
-                                minimumRequiredOptions: val,
-                            });
-                        }}
-                        onBlur={() => {
-                            if (props.element.minimumRequiredOptions == null || props.element.minimumRequiredOptions === 0) {
-                                props.onPatch({
-                                    required: false,
-                                });
-                            }
-                        }}
-                        error={minRequiredError}
-                    />
-                </FormControl>
+                <NumberFieldComponent
+                    label="Erforderliche Mindestanzahl"
+                    value={props.element.minimumRequiredOptions ?? 1}
+                    onChange={val => props.onPatch({
+                        required: val === 0 ? false : props.element.required,
+                        minimumRequiredOptions: val === 0 ? undefined : val,
+                    })}
+                    error={minRequiredError ? 'Sie fordern mehr Optionen als Sie definiert haben.' : undefined}
+                    hint="Geben Sie 0 ein, um keine Mindestanzahl zu fordern."
+                />
             }
         </>
     );
--- app/src/components/multi-checkbox-field/multi-checkbox-field.component.validator.ts
+++ app/src/components/multi-checkbox-field/multi-checkbox-field.component.validator.ts
@@ -1,5 +1,6 @@
 import {BaseInputElementValidator} from '../../validators/base-input-element-validator';
 import {MultiCheckboxFieldElement} from '../../models/elements/form/input/multi-checkbox-field-element';
+import {humanizeNumber, pluralize} from "../../utils/huminization-utils";
 
 export class MultiCheckboxFieldComponentValidator extends BaseInputElementValidator<string[], MultiCheckboxFieldElement> {
     protected checkEmpty(comp: MultiCheckboxFieldElement, value: string[]): boolean {
@@ -7,12 +8,14 @@ export class MultiCheckboxFieldComponentValidator extends BaseInputElementValida
     }
 
     protected getEmptyErrorText(comp: MultiCheckboxFieldElement): string {
-        return `Dies ist eine Pflichtangabe. Bitte wählen Sie mindestens ${comp.minimumRequiredOptions != null && comp.minimumRequiredOptions === 1 ? 'eine Option' : `${comp.minimumRequiredOptions?.toFixed()} Optionen`} aus.`;
+        const minRequired = comp.minimumRequiredOptions ?? 1;
+
+        return `Dies ist eine Pflichtangabe. Bitte wählen Sie mindestens ${humanizeNumber(minRequired)} ${pluralize(minRequired, 'Option', 'Optionen')} aus.`;
     }
 
     protected makeSpecificErrors(comp: MultiCheckboxFieldElement, value: string[] | undefined, userInput: any): string | null {
         if (value != null && comp.minimumRequiredOptions != null && value.length < comp.minimumRequiredOptions) {
-            return `Dies ist eine Pflichtangabe. Bitte wählen Sie mindestens ${comp.minimumRequiredOptions.toFixed()} Optionen aus.`;
+            return `Dies ist eine Pflichtangabe. Bitte wählen Sie mindestens ${humanizeNumber(comp.minimumRequiredOptions)} ${pluralize(comp.minimumRequiredOptions, 'Option', 'Optionen')} aus.`;
         }
         return null;
     }
