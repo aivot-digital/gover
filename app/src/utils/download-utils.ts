01de11da81f887a8d698384f27a446f228dfb3a0
Finn Moritz Bätzel <code@mobaetzel.de>
2023-05-28 18:46:08 +0200

    fix: Fix export customer inputs to exclude files

--- app/src/utils/download-utils.ts
+++ app/src/utils/download-utils.ts
@@ -21,3 +21,34 @@ export function downloadConfigFile(application?: Application): void {
         );
     }
 }
+
+export function uploadTextFile(accepts: string): Promise<string | null> {
+    return new Promise((resolve, reject) => {
+
+        const input = document.createElement('input');
+        input.type = 'file';
+        input.accept = accepts;
+        input.addEventListener('change', event => {
+            if (input.files != null) {
+                const reader = new FileReader();
+                reader.onload = (evt) => {
+                    const value = evt.target?.result;
+                    if (value != null) {
+                        resolve(value.toString());
+                    } else {
+                        reject(new Error('Empty reader result'));
+                    }
+                };
+                reader.onerror = reject;
+                reader.readAsText(input.files[0], 'UTF-8');
+            }
+            input.remove();
+        });
+        input.click();
+    });
+}
+
+export function uploadObjectFile<T>(accepts: string): Promise<T | null> {
+    return uploadTextFile(accepts)
+        .then(res => res != null ? JSON.parse(res) : null);
+}
