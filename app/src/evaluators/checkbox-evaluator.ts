bc4df10dec74a4cecd6444168b7a01e7603571c9
Finn Moritz Bätzel <code@mobaetzel.de>
2023-05-29 12:14:16 +0200

    fix: Fix null values for checkbox when equaling false

--- app/src/evaluators/checkbox-evaluator.ts
+++ app/src/evaluators/checkbox-evaluator.ts
@@ -1,19 +1,35 @@
 import {ConditionOperator} from "../data/condition-operator";
 import {BaseEvaluator} from "./base-evaluator";
 
+const falseValue = 'Nein (False)';
+const trueValue = 'Ja (True)';
+
 function transformValue(val: any) {
     if (typeof val === 'boolean') {
-        return val ? 'Ja (True)' : 'Nein (False)';
+        return val ? trueValue : falseValue;
     }
     return val;
 }
 
 export const CheckboxEvaluator: BaseEvaluator<boolean> = {
     [ConditionOperator.Equals]: (valueA, valueB) => {
-        return valueA == valueB || transformValue(valueA) === valueB;
+        if (valueA == null && valueB == null) {
+            return true;
+        }
+        if (valueB === falseValue && (valueA == null || !valueA)) {
+            return true;
+        }
+        return valueB === trueValue && valueA != null && valueA;
+
     },
     [ConditionOperator.NotEquals]: (valueA, valueB) => {
-        return valueA != valueB || transformValue(valueA) !== valueB;
+        if (valueA == null && valueB == null) {
+            return false;
+        }
+        if (valueB === falseValue && (valueA == null || !valueA)) {
+            return false;
+        }
+        return!(valueB === trueValue && valueA != null && valueA);
     },
 
     [ConditionOperator.Empty]: (valueA, _) => {
