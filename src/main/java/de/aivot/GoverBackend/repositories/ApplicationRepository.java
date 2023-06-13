aaad94e56b3bd81428bed16ffbc16af32a94327e
Finn Moritz Bätzel <code@mobaetzel.de>
2023-06-13 09:14:19 +0200

    fix: Fixed fetching public applications updating the record

--- src/main/java/de/aivot/GoverBackend/repositories/ApplicationRepository.java
+++ src/main/java/de/aivot/GoverBackend/repositories/ApplicationRepository.java
@@ -1,22 +1,23 @@
 package de.aivot.GoverBackend.repositories;
 
 import de.aivot.GoverBackend.models.entities.Application;
-import javax.transaction.Transactional;
+
 
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Collection;
 import java.util.Optional;
 
 @RepositoryRestResource(collectionResourceRel = "applications", path = "applications")
 public interface ApplicationRepository extends PagingAndSortingRepository<Application, Long>, CrudRepository<Application, Long> {
-    @Transactional
+    @Transactional(readOnly = true)
     Optional<Application> getBySlugAndVersion(String slug, String version);
 
-    @Transactional
+    @Transactional(readOnly = true)
     @Query("SELECT app FROM Application app WHERE app.status = 2")
     Collection<Application> findPublishedApplications();
 }
