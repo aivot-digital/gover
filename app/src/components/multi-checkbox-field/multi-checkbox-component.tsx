4bb06e0115d7c1f5bef2bc958a489a882b6edf4f
Finn Moritz Bätzel <code@mobaetzel.de>
2023-06-14 15:25:35 +0200

    fix: Fix multi checkbox to respect the order of options when setting the value

--- app/src/components/multi-checkbox-field/multi-checkbox-component.tsx
+++ app/src/components/multi-checkbox-field/multi-checkbox-component.tsx
@@ -23,6 +23,22 @@ export function MultiCheckboxComponent({
                                            disabled,
                                            required,
                                        }: MultiCheckboxComponentProps) {
+
+    const handleChange = (option: string) => {
+        if (value == null || value.length === 0) {
+            onChange([option]);
+            return;
+        }
+
+        if (value.includes(option)) {
+            const splicedList = value.filter(v => v !== option);
+            onChange(splicedList.length > 0 ? splicedList : undefined);
+            return;
+        }
+
+        onChange(options.filter(opt => value.includes(opt) || opt === option));
+    };
+
     return (
         <FormControl
             error={error != null}
@@ -44,23 +60,7 @@ export function MultiCheckboxComponent({
                             control={
                                 <Checkbox
                                     checked={(value ?? []).includes(option)}
-                                    onChange={() => {
-                                            const list = value ?? [];
-
-                                            if (list.includes(option)) {
-                                                const updatedList = list.filter((elem: string) => elem !== option);
-                                                if (updatedList.length === 0) {
-                                                    onChange(undefined);
-                                                } else {
-                                                    onChange(updatedList);
-                                                }
-                                            } else {
-                                                onChange([
-                                                    ...list,
-                                                    option,
-                                                ]);
-                                            }
-                                    }}
+                                    onChange={() => handleChange(option)}
                                     disabled={disabled}
                                 />
                             }
--- app/src/components/multi-checkbox-field/multi-checkbox-field.component.view.tsx
+++ app/src/components/multi-checkbox-field/multi-checkbox-field.component.view.tsx
@@ -1,6 +1,6 @@
-import {Checkbox, FormControl, FormControlLabel, FormGroup, FormHelperText, FormLabel} from '@mui/material';
 import {MultiCheckboxFieldElement} from '../../models/elements/form/input/multi-checkbox-field-element';
 import {BaseViewProps} from "../../views/base-view";
+import {MultiCheckboxComponent} from "./multi-checkbox-component";
 
 export function MultiCheckboxFieldComponentView({
                                                     element,
@@ -9,65 +9,17 @@ export function MultiCheckboxFieldComponentView({
                                                     setValue
                                                 }: BaseViewProps<MultiCheckboxFieldElement, string[]>) {
     return (
-        <FormControl
-            error={error != null}
-        >
-            {
-                element.label &&
-                <FormLabel
-                    component="legend"
-                    disabled={element.disabled}
-                >
-                    {element.label} {element.required && '*'}
-                </FormLabel>
-            }
-            <FormGroup>
-                {
-                    (element.options ?? []).map(option => (
-                        <FormControlLabel
-                            key={option}
-                            control={
-                                <Checkbox
-                                    checked={(value ?? []).includes(option)}
-                                    onChange={() => {
-                                        if (element.id != null) {
-                                            const list = value ?? [];
-
-                                            if (list.includes(option)) {
-                                                const updatedList = list.filter((elem: string) => elem !== option);
-                                                if (updatedList.length === 0) {
-                                                    setValue(undefined);
-                                                } else {
-                                                    setValue(updatedList);
-                                                }
-                                            } else {
-                                                setValue([
-                                                    ...list,
-                                                    option,
-                                                ]);
-                                            }
-                                        }
-                                    }}
-                                    disabled={element.disabled}
-                                />
-                            }
-                            label={option}
-                        />
-                    ))
-                }
-            </FormGroup>
-            {
-                (element.hint != null || error != null) &&
-                <FormHelperText sx={{ml: 0}}>
-                    {
-                        error == null && element.hint
-                    }
-                    {
-                        error != null &&
-                        <span>{error}</span>
-                    }
-                </FormHelperText>
-            }
-        </FormControl>
+        <>
+            <MultiCheckboxComponent
+                label={element.label ?? ''}
+                value={value ?? undefined}
+                onChange={setValue}
+                options={element.options ?? []}
+                disabled={element.disabled}
+                required={element.required}
+                error={error}
+                hint={element.hint}
+            />
+        </>
     );
 }
