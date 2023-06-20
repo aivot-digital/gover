219d8b52d7873e1a00d1a0794e89969b186fda66
Finn Moritz Bätzel <code@mobaetzel.de>
2023-06-20 11:02:47 +0200

    fix: Fix missing child test

---
+++ app/src/utils/is-child-of.ts
@@ -0,0 +1,18 @@
+import {AnyElement} from "../models/elements/any-element";
+import {AnyElementWithChildren, isAnyElementWithChildren} from "../models/elements/any-element-with-children";
+
+export function isChildOf(target: AnyElement, parent: AnyElementWithChildren): boolean {
+    for (const c of parent.children) {
+        if (c.id === target.id) {
+            return true;
+        }
+
+        if (isAnyElementWithChildren(c)) {
+            if (isChildOf(target, c)) {
+                return true
+            }
+        }
+    }
+
+    return false;
+}
