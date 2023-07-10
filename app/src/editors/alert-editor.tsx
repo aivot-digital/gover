f2f0f36c5dd2709159ad57c70fa0febf927eeeb6
Finn Moritz Bätzel <code@mobaetzel.de>
2023-07-10 09:40:56 +0200

    feat: Changed wording for alert editor
    #861n1cvyy

--- app/src/editors/alert-editor.tsx
+++ app/src/editors/alert-editor.tsx
@@ -1,47 +1,53 @@
-import {AlertColor} from '@mui/material';
-import {AlertElement} from "../models/elements/form/content/alert-element";
-import {BaseEditor} from "./base-editor";
-import {TextFieldComponent} from "../components/text-field/text-field-component";
-import {SelectFieldComponent} from "../components/select-field/select-field-component";
+import { type AlertColor } from '@mui/material';
+import { type AlertElement } from '../models/elements/form/content/alert-element';
+import { type BaseEditor } from './base-editor';
+import { TextFieldComponent } from '../components/text-field/text-field-component';
+import { SelectFieldComponent } from '../components/select-field/select-field-component';
 
 const colors = [
-    ['success', 'Erfolg (Grün)'],
-    ['info', 'Information (Blau)'],
-    ['warning', 'Warnung (Orange)'],
-    ['error', 'Fehler (Rot)'],
+    ['success', 'Erfolg'],
+    ['info', 'Information'],
+    ['warning', 'Warnung'],
+    ['error', 'Fehler'],
 ];
 
-export const AlertEditor: BaseEditor<AlertElement> = ({element, onPatch}) => {
+export const AlertEditor: BaseEditor<AlertElement> = ({ element, onPatch }) => {
     return (
         <>
             <TextFieldComponent
                 value={element.title}
                 label="Titel"
-                onChange={val => onPatch({
-                    title: val,
-                })}
+                onChange={(val) => {
+                    onPatch({
+                        title: val,
+                    });
+                }}
             />
 
             <TextFieldComponent
                 value={element.text}
                 label="Hinweis"
                 multiline
-                onChange={val => onPatch({
-                    text: val,
-                })}
+                onChange={(val) => {
+                    onPatch({
+                        text: val,
+                    });
+                }}
             />
 
             <SelectFieldComponent
                 label="Hinweistyp"
                 value={element.alertType ?? 'info'}
-                onChange={val => onPatch({
-                    alertType: val as AlertColor,
-                })}
+                onChange={(val) => {
+                    onPatch({
+                        alertType: val as AlertColor,
+                    });
+                }}
                 options={colors.map(([type, label]) => ({
-                    label: label,
+                    label,
                     value: type,
                 }))}
             />
         </>
     );
-}
+};
