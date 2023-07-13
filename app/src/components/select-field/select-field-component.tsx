f28a25a957c98affcb3df18c71bba60e526acc38
Finn Moritz Bätzel <code@mobaetzel.de>
2023-07-13 11:18:47 +0200

    fix: Hide "Keine Auswahl" in select-field-component.tsx
    #861n1fkxe

--- app/src/components/select-field/select-field-component.tsx
+++ app/src/components/select-field/select-field-component.tsx
@@ -1,29 +1,31 @@
-import {MenuItem, TextField} from '@mui/material';
-import {isStringNullOrEmpty} from "../../utils/string-utils";
-import {SelectFieldComponentProps} from "./select-field-component-props";
+import React from 'react';
+import { MenuItem, TextField } from '@mui/material';
+import { isStringNullOrEmpty } from '../../utils/string-utils';
+import { type SelectFieldComponentProps } from './select-field-component-props';
 
 export function SelectFieldComponent({
-                                         label,
-                                         placeholder,
-                                         hint,
-                                         disabled,
-                                         required,
-                                         error,
-                                         value,
-                                         onChange,
-                                         options,
-                                     }: SelectFieldComponentProps) {
+    label,
+    placeholder,
+    hint,
+    disabled,
+    required,
+    error,
+    value,
+    onChange,
+    options,
+}: SelectFieldComponentProps): JSX.Element {
     const val = value ?? '';
+
     return (
         <TextField
             select
             fullWidth
-            label={label + (required ? ' *' : '')}
+            label={label + ((required ?? false) ? ' *' : '')}
             error={error != null}
             helperText={error != null ? error : hint}
             placeholder={placeholder}
             value={val}
-            onChange={event => {
+            onChange={(event) => {
                 if (isStringNullOrEmpty(event.target.value)) {
                     onChange(undefined);
                 } else {
@@ -33,7 +35,8 @@ export function SelectFieldComponent({
             disabled={disabled ?? false}
         >
             {
-                !required &&
+                !(required ?? false) &&
+                options.length > 0 &&
                 <MenuItem
                     value={''}
                 >
@@ -49,9 +52,10 @@ export function SelectFieldComponent({
                     <i>Keine Optionen vorhanden</i>
                 </MenuItem>
             }
+
             {
                 options
-                    .map(option => (
+                    .map((option) => (
                         <MenuItem
                             key={option.value}
                             value={option.value}
