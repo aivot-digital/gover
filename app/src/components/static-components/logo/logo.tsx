85de8d52bd5b628a8b33ce312df897099739fd31
Finn Moritz Bätzel <code@mobaetzel.de>
2023-07-19 08:57:24 +0200

    fix: Fix logo loading correctly when unauthenticated

--- app/src/components/static-components/logo/logo.tsx
+++ app/src/components/static-components/logo/logo.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {useAppSelector} from '../../../hooks/use-app-selector';
 import {selectSystemConfigValue} from '../../../slices/system-config-slice';
 import {SystemConfigKeys} from '../../../data/system-config-keys';
@@ -16,6 +16,10 @@ export function Logo(props: LogoProps): JSX.Element {
     const [imageFailed, setImageFailed] = useState(false);
     const name = useAppSelector(selectSystemConfigValue(SystemConfigKeys.provider.name));
     const logo = useAppSelector(selectSystemConfigValue(SystemConfigKeys.system.logo));
+
+    useEffect(() => {
+        setImageFailed(false);
+    }, [logo]);
 
     if (imageFailed) {
         return (
