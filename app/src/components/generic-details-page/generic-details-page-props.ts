fa5cd3fd3de0d9876c0314e68887c84b4861bd10
Daniel Kratz <daniel.kratz@aivot.de>
2025-04-25 01:09:46 +0200

    feat: allow dynamic tab callback function for GenericDetailsPage
    CU-86c34dmn3

--- app/src/components/generic-details-page/generic-details-page-props.ts
+++ app/src/components/generic-details-page/generic-details-page-props.ts
@@ -2,18 +2,20 @@ import {BadgeProps} from '@mui/material';
 import {Api} from '../../hooks/use-api';
 import {GenericPageHeaderProps} from '../generic-page-header/generic-page-header-props';
 
+type TabConfig<ItemType> = {
+    path: string;
+    label: string;
+    badge?: BadgeProps;
+    isDisabled?: (item: ItemType | undefined) => boolean;
+};
+
 export interface GenericDetailsPageProps<ItemType, ID, AdditionalData> {
     getTabTitle: (item: ItemType) => string;
     header: Omit<GenericPageHeaderProps, 'isBusy'>;
     initializeItem: (api: Api) => ItemType;
     fetchData: (api: Api, id: ID) => Promise<ItemType>;
     fetchAdditionalData?: AdditionalDataFetchObject<AdditionalData, ID>;
-    tabs: {
-        path: string;
-        label: string;
-        badge?: BadgeProps;
-        isDisabled?: (item: ItemType | undefined) => boolean;
-    }[];
+    tabs: TabConfig<ItemType>[] | ((item: ItemType | undefined) => TabConfig<ItemType>[]);
     idParam?: string;
     // parentLink is used for links to the list pages on 404 errors
     parentLink?: {
