96217388afad7e8370af4a13b348783de8825944
Finn Moritz Bätzel <finn.baetzel@aivot.de>
2025-05-16 16:02:12 +0200

    fix: fix identity provider relation building

--- src/main/resources/db/migration/V6_1_2__default_identity_provider_bayern_id_relations.sql
+++ src/main/resources/db/migration/V6_1_2__default_identity_provider_bayern_id_relations.sql
@@ -1,5 +1,5 @@
 update forms as fms
 set identity_providers = fms.identity_providers || json_build_array(json_build_object(
-        'identityProviderKey', (select idps.key from identity_providers as idps where idps.type = 1 limit 1),
+        'identityProviderKey', (select idps.key from identity_providers as idps where idps.type = 1 and idps.is_test_provider = False limit 1),
         'additionalScopes', json_build_array('level' || fms.bayern_id_level)))::jsonb
 where fms.bayern_id_enabled;
