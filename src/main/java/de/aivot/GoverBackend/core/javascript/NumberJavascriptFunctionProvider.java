062b4dcd35442cb4b30dbf515691da2e920a85d7
Finn Moritz Bätzel <finn.baetzel@aivot.de>
2025-09-22 15:12:49 +0200

    fix: fix typo in number js module label

--- src/main/java/de/aivot/GoverBackend/core/javascript/NumberJavascriptFunctionProvider.java
+++ src/main/java/de/aivot/GoverBackend/core/javascript/NumberJavascriptFunctionProvider.java
@@ -14,7 +14,7 @@ public class NumberJavascriptFunctionProvider implements JavascriptFunctionProvi
 
     @Override
     public String getLabel() {
-        return "zahlenangaben";
+        return "Zahlenangaben";
     }
 
     @Override
