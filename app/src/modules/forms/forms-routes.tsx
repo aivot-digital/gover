04d33027c12768405730d696978c1da8097e4e9a
Finn Moritz Bätzel <finn.baetzel@aivot.de>
2025-09-22 11:31:08 +0200

    fix: fix broken import for forms-routes.tsx

--- app/src/modules/forms/forms-routes.tsx
+++ app/src/modules/forms/forms-routes.tsx
@@ -1,7 +1,7 @@
 import {RouteObject} from 'react-router-dom';
-import {FormsListPage} from './pages/list/forms-list-page';
 import {FormEditPage} from '../../pages/staff-pages/application-pages/form-edit-page';
 import React from 'react';
+import {FormsListPage} from './pages/list/forms-list-page/forms-list-page';
 
 export const formsRoutes: RouteObject[] = [
     {
