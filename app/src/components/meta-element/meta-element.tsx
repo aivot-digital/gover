f32c51a0adf329357fe4293db3420a65002bf985
Finn Moritz Bätzel <finn.baetzel@aivot.de>
2025-09-28 11:55:34 +0200

    feat: make favicon use default favicon endpoint

--- app/src/components/meta-element/meta-element.tsx
+++ app/src/components/meta-element/meta-element.tsx
@@ -1,9 +1,5 @@
-import React, {useEffect } from 'react';
+import React, {useEffect} from 'react';
 import {useTheme} from '@mui/material';
-import {useAppSelector} from '../../hooks/use-app-selector';
-import {selectSystemConfigValue} from '../../slices/system-config-slice';
-import {SystemConfigKeys} from '../../data/system-config-keys';
-import {AssetsApiService} from '../../modules/assets/assets-api-service';
 
 interface MetaElementProps {
     title?: string;
@@ -12,8 +8,6 @@ interface MetaElementProps {
 
 export function MetaElement({title, titlePrefix}: MetaElementProps) {
     const theme = useTheme();
-
-    const favicon = useAppSelector(selectSystemConfigValue(SystemConfigKeys.system.favicon));
 
     useEffect(() => {
         const root = document.documentElement;
@@ -24,7 +18,7 @@ export function MetaElement({title, titlePrefix}: MetaElementProps) {
 
     return (
         <>
-            <meta charSet="utf-8"/>
+            <meta charSet="utf-8" />
             <title>
                 {(titlePrefix ?? 'Gover') + (title ? ` - ${title}` : '')}
             </title>
@@ -36,25 +30,34 @@ export function MetaElement({title, titlePrefix}: MetaElementProps) {
                 name="generator"
                 content="Gover – Die quelloffene Plattform für Ende-zu-Ende digitalisierte Antragsprozesse. (gover.digital)"
             />
-            {favicon ? (
-                <link rel="icon"
-                      href={AssetsApiService.useAssetLink(favicon)}/>
-            ) : [
-                <link key="apple-touch-icon"
-                      rel="apple-touch-icon"
-                      sizes="180x180"
-                      href={`${import.meta.env.BASE_URL}/apple-touch-icon.png`}/>,
-                <link key="icon-32x32"
-                      rel="icon"
-                      type="image/png"
-                      sizes="32x32"
-                      href={`${import.meta.env.BASE_URL}/favicon-32x32.png`}/>,
-                <link key="icon-16x16"
-                      rel="icon"
-                      type="image/png"
-                      sizes="16x16"
-                      href={`${import.meta.env.BASE_URL}/favicon-16x16.png`}/>
-            ]}
+
+            <link
+                key="apple-touch-icon"
+                rel="apple-touch-icon"
+                sizes="180x180"
+                href={`${import.meta.env.BASE_URL}/apple-touch-icon.png`}
+            />
+
+            <link
+                key="icon-32x32"
+                rel="icon"
+                type="image/png"
+                sizes="32x32"
+                href={`${import.meta.env.BASE_URL}/favicon-32x32.png`}
+            />
+
+            <link
+                key="icon-16x16"
+                rel="icon"
+                type="image/png"
+                sizes="16x16"
+                href={`${import.meta.env.BASE_URL}/favicon-16x16.png`}
+            />
+
+            <link
+                rel="icon"
+                href="/api/public/system/favicon/"
+            />
         </>
     );
 }
