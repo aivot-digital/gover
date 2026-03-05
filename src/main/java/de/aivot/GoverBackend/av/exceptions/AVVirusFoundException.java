ff03fd52e36f8efd865291e24341669443b7e2cd
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-03-05 10:22:57 +0100

    feat: simplify error message in AVVirusFoundException by removing support contact reference

--- src/main/java/de/aivot/GoverBackend/av/exceptions/AVVirusFoundException.java
+++ src/main/java/de/aivot/GoverBackend/av/exceptions/AVVirusFoundException.java
@@ -8,7 +8,7 @@ public class AVVirusFoundException extends ResponseException {
     public AVVirusFoundException(String filename) {
         super(
                 HttpStatus.BAD_REQUEST,
-                "In der hochgeladenen Datei wurde Schadsoftware erkannt. Der Upload wurde aus Sicherheitsgründen blockiert. Bitte bereinigen oder ersetzen Sie die Datei und laden Sie sie erneut hoch. Bei Rückfragen wenden Sie sich an den technischen Support.",
+                "In der hochgeladenen Datei wurde Schadsoftware erkannt. Der Upload wurde aus Sicherheitsgründen blockiert. Bitte bereinigen oder ersetzen Sie die Datei und laden Sie sie erneut hoch.",
                 String.format("Die Datei %s wurde vom Virenscanner als unsicher eingestuft.", StringUtils.quote(filename))
         );
     }
@@ -16,7 +16,7 @@ public class AVVirusFoundException extends ResponseException {
     public AVVirusFoundException(String filename, String scannerResponse) {
         super(
                 HttpStatus.BAD_REQUEST,
-                "In der hochgeladenen Datei wurde Schadsoftware erkannt. Der Upload wurde aus Sicherheitsgründen blockiert. Bitte bereinigen oder ersetzen Sie die Datei und laden Sie sie erneut hoch. Bei Rückfragen wenden Sie sich an den technischen Support.",
+                "In der hochgeladenen Datei wurde Schadsoftware erkannt. Der Upload wurde aus Sicherheitsgründen blockiert. Bitte bereinigen oder ersetzen Sie die Datei und laden Sie sie erneut hoch.",
                 String.format("Die Datei %s wurde vom Virenscanner als unsicher eingestuft. Scanner-Antwort: %s", StringUtils.quote(filename), scannerResponse)
         );
     }
