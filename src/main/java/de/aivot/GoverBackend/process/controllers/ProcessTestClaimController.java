f6ccb05effa37d528b1f82b42eeb7f00b08d0418
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-03-07 11:18:30 +0100

    refactor: enhance permission checks in ProcessTestClaimController and add repository methods for process access validation

--- src/main/java/de/aivot/GoverBackend/process/controllers/ProcessTestClaimController.java
+++ src/main/java/de/aivot/GoverBackend/process/controllers/ProcessTestClaimController.java
@@ -9,12 +9,16 @@ import de.aivot.GoverBackend.permissions.services.PermissionService;
 import de.aivot.GoverBackend.process.entities.ProcessTestClaimEntity;
 import de.aivot.GoverBackend.process.filters.ProcessTestClaimFilter;
 import de.aivot.GoverBackend.process.permissions.ProcessPermissionProvider;
+import de.aivot.GoverBackend.process.repositories.ProcessTestClaimRepository;
 import de.aivot.GoverBackend.process.services.ProcessTestClaimService;
 import de.aivot.GoverBackend.user.entities.UserEntity;
 import de.aivot.GoverBackend.user.services.UserService;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.annotation.Nonnull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.domain.Specification;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -28,25 +32,97 @@ import org.springframework.web.bind.annotation.RestController;
 @SecurityRequirement(name = OpenApiConfiguration.Security)
 public class ProcessTestClaimController extends GenericCrudController<ProcessTestClaimEntity, Integer, ProcessTestClaimFilter> {
     private final PermissionService permissionService;
+    private final ProcessTestClaimService processTestClaimService;
+    private final ProcessTestClaimRepository processTestClaimRepository;
 
     public ProcessTestClaimController(AuditService auditService,
                                       UserService userService,
                                       ProcessTestClaimService service,
-                                      PermissionService permissionService) {
+                                      PermissionService permissionService,
+                                      ProcessTestClaimRepository processTestClaimRepository) {
         super(
                 auditService.createScopedAuditService(ProcessTestClaimController.class),
                 userService,
                 service
         );
         this.permissionService = permissionService;
+        this.processTestClaimService = service;
+        this.processTestClaimRepository = processTestClaimRepository;
     }
-
-    // TODO: Limit list to accessible process definitions and perform permission check for all other endpoints
 
     @Override
     protected void checkListPermissions(@Nonnull UserEntity user) throws ResponseException {
-        permissionService
-                .testInAnyDepartmentPermission(user.getId(), ProcessPermissionProvider.PROCESS_DEFINITION_READ);
+        // List permissions are checked in performList to support scoped domain filtering.
+    }
+
+    @Override
+    protected Page<ProcessTestClaimEntity> performList(@Nonnull UserEntity user,
+                                                       @Nonnull Pageable pageable,
+                                                       @Nonnull ProcessTestClaimFilter filter) throws ResponseException {
+        if (hasGlobalReadPermission(user.getId())) {
+            return super.performList(user, pageable, filter);
+        }
+
+        var accessibleProcessIds = processTestClaimRepository.getProcessIdsWithPermission(
+                user.getId(),
+                ProcessPermissionProvider.PROCESS_DEFINITION_READ
+        );
+        if (accessibleProcessIds.isEmpty()) {
+            return Page.empty(pageable);
+        }
+
+        Specification<ProcessTestClaimEntity> specification = filter.build();
+        Specification<ProcessTestClaimEntity> processScopeSpecification = (root, query, criteriaBuilder) ->
+                root.get("processId").in(accessibleProcessIds);
+
+        if (specification == null) {
+            specification = processScopeSpecification;
+        } else {
+            specification = specification.and(processScopeSpecification);
+        }
+
+        return processTestClaimService.performList(pageable, specification, filter);
+    }
+
+    @Override
+    protected void checkRetrievePermissions(@Nonnull UserEntity execUser,
+                                            @Nonnull Integer itemid) throws ResponseException {
+        var entity = processTestClaimService
+                .retrieve(itemid)
+                .orElseThrow(ResponseException::notFound);
+        if (!canReadProcess(execUser.getId(), entity.getProcessId())) {
+            throw ResponseException.forbidden();
+        }
+    }
+
+    @Override
+    protected void checkCreatePermissions(@Nonnull UserEntity execUser,
+                                          @Nonnull ProcessTestClaimEntity newItem) throws ResponseException {
+        if (!canManageProcess(execUser.getId(), newItem.getProcessId())) {
+            throw ResponseException.forbidden();
+        }
+    }
+
+    @Override
+    protected void checkUpdatePermission(@Nonnull UserEntity execUser,
+                                         @Nonnull Integer itemid) throws ResponseException {
+        var entity = processTestClaimService
+                .retrieve(itemid)
+                .orElseThrow(ResponseException::notFound);
+        if (!canManageProcess(execUser.getId(), entity.getProcessId())) {
+            throw ResponseException.forbidden();
+        }
+    }
+
+    @Override
+    protected void checkDeletePermission(@Nonnull UserEntity execUser,
+                                         @Nonnull Integer itemid) throws ResponseException {
+        var entity = processTestClaimService
+                .retrieve(itemid)
+                .orElseThrow(ResponseException::notFound);
+        if (!canManageProcess(execUser.getId(), entity.getProcessId())) {
+            throw ResponseException.forbidden();
+        }
     }
 
     @Override
@@ -59,5 +135,39 @@ public class ProcessTestClaimController extends GenericCrudController<ProcessTes
     @Override
     protected Integer getIdForEntity(ProcessTestClaimEntity entity) {
         return entity.getId();
+    }
+
+    private boolean hasGlobalReadPermission(@Nonnull String userId) {
+        return permissionService.hasSystemPermission(
+                userId,
+                ProcessPermissionProvider.PROCESS_DEFINITION_READ
+        );
+    }
+
+    private boolean hasGlobalManagePermission(@Nonnull String userId) {
+        return permissionService.hasSystemPermission(
+                userId,
+                ProcessPermissionProvider.PROCESS_DEFINITION_PUBLISH_TEST
+        );
+    }
+
+    private boolean canReadProcess(@Nonnull String userId,
+                                   @Nonnull Integer processId) {
+        return hasGlobalReadPermission(userId)
+                || processTestClaimRepository.hasProcessPermission(
+                userId,
+                processId,
+                ProcessPermissionProvider.PROCESS_DEFINITION_READ
+        );
+    }
+
+    private boolean canManageProcess(@Nonnull String userId,
+                                     @Nonnull Integer processId) {
+        return hasGlobalManagePermission(userId)
+                || processTestClaimRepository.hasProcessPermission(
+                userId,
+                processId,
+                ProcessPermissionProvider.PROCESS_DEFINITION_PUBLISH_LOCAL
+        );
     }
 }
