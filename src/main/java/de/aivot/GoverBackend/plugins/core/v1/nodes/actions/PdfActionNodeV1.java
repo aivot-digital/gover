34c5ec27ad43f690205353c3dae672afd729b1d3
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-03-07 12:51:14 +0100

    refactor: enhance PdfActionNodeV1 with asset-based content support and streamlined configuration options

--- src/main/java/de/aivot/GoverBackend/plugins/core/v1/nodes/actions/PdfActionNodeV1.java
+++ src/main/java/de/aivot/GoverBackend/plugins/core/v1/nodes/actions/PdfActionNodeV1.java
@@ -1,38 +1,48 @@
 package de.aivot.GoverBackend.plugins.core.v1.nodes.actions;
 
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import de.aivot.GoverBackend.asset.repositories.VStorageIndexItemWithAssetRepository;
+import de.aivot.GoverBackend.asset.services.AssetService;
+import de.aivot.GoverBackend.asset.services.VStorageIndexItemWithAssetService;
 import de.aivot.GoverBackend.elements.annotations.ElementPOJOBindingProperty;
 import de.aivot.GoverBackend.elements.annotations.InputElementPOJOBinding;
 import de.aivot.GoverBackend.elements.annotations.LayoutElementPOJOBinding;
 import de.aivot.GoverBackend.elements.exceptions.ElementDataConversionException;
+import de.aivot.GoverBackend.elements.models.elements.ElementVisibilityFunctions;
+import de.aivot.GoverBackend.elements.models.elements.form.input.CodeInputElement;
+import de.aivot.GoverBackend.elements.models.elements.form.input.RadioInputElement;
+import de.aivot.GoverBackend.elements.models.elements.form.input.RadioInputElementOption;
+import de.aivot.GoverBackend.elements.models.elements.form.input.SelectInputElement;
 import de.aivot.GoverBackend.elements.models.elements.layout.ConfigLayoutElement;
 import de.aivot.GoverBackend.elements.utils.ElementPOJOMapper;
 import de.aivot.GoverBackend.enums.ElementType;
 import de.aivot.GoverBackend.lib.exceptions.ResponseException;
+import de.aivot.GoverBackend.nocode.models.NoCodeExpression;
+import de.aivot.GoverBackend.nocode.models.NoCodeReference;
+import de.aivot.GoverBackend.nocode.models.NoCodeStaticValue;
 import de.aivot.GoverBackend.plugins.core.Core;
+import de.aivot.GoverBackend.plugins.core.v1.operators.common.NoCodeEqualsOperator;
 import de.aivot.GoverBackend.process.entities.ProcessInstanceAttachmentEntity;
 import de.aivot.GoverBackend.process.enums.ProcessNodeType;
 import de.aivot.GoverBackend.process.exceptions.ProcessNodeExecutionException;
 import de.aivot.GoverBackend.process.exceptions.ProcessNodeExecutionExceptionInvalidConfiguration;
 import de.aivot.GoverBackend.process.exceptions.ProcessNodeExecutionExceptionMissingValue;
 import de.aivot.GoverBackend.process.exceptions.ProcessNodeExecutionExceptionUnknown;
-import de.aivot.GoverBackend.process.models.ProcessNodeDefinition;
-import de.aivot.GoverBackend.process.models.ProcessNodeDefinitionContextConfig;
-import de.aivot.GoverBackend.process.models.ProcessNodeExecutionContextInit;
-import de.aivot.GoverBackend.process.models.ProcessNodeExecutionResult;
-import de.aivot.GoverBackend.process.models.ProcessNodeExecutionResultTaskCompleted;
-import de.aivot.GoverBackend.process.models.ProcessNodeOutput;
-import de.aivot.GoverBackend.process.models.ProcessNodePort;
-import de.aivot.GoverBackend.process.services.ProcessInstanceAttachmentService;
+import de.aivot.GoverBackend.process.models.*;
 import de.aivot.GoverBackend.process.services.ProcessDataService;
+import de.aivot.GoverBackend.process.services.ProcessInstanceAttachmentService;
 import de.aivot.GoverBackend.services.PdfService;
+import de.aivot.GoverBackend.storage.services.StorageService;
 import de.aivot.GoverBackend.utils.StringUtils;
 import jakarta.annotation.Nonnull;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
 import java.net.URISyntaxException;
+import java.nio.charset.StandardCharsets;
 import java.util.HashMap;
 import java.util.List;
+import java.util.UUID;
 
 @Component
 public class PdfActionNodeV1 implements ProcessNodeDefinition {
@@ -50,13 +60,25 @@ public class PdfActionNodeV1 implements ProcessNodeDefinition {
     private final PdfService pdfService;
     private final ProcessDataService processDataService;
     private final ProcessInstanceAttachmentService processInstanceAttachmentService;
+    private final AssetService assetService;
+    private final VStorageIndexItemWithAssetService vStorageIndexItemWithAssetService;
+    private final VStorageIndexItemWithAssetRepository vStorageIndexItemWithAssetRepository;
+    private final StorageService storageService;
 
     public PdfActionNodeV1(PdfService pdfService,
                            ProcessDataService processDataService,
-                           ProcessInstanceAttachmentService processInstanceAttachmentService) {
+                           ProcessInstanceAttachmentService processInstanceAttachmentService,
+                           AssetService assetService,
+                           VStorageIndexItemWithAssetService vStorageIndexItemWithAssetService,
+                           VStorageIndexItemWithAssetRepository vStorageIndexItemWithAssetRepository,
+                           StorageService storageService) {
         this.pdfService = pdfService;
         this.processDataService = processDataService;
         this.processInstanceAttachmentService = processInstanceAttachmentService;
+        this.assetService = assetService;
+        this.vStorageIndexItemWithAssetService = vStorageIndexItemWithAssetService;
+        this.vStorageIndexItemWithAssetRepository = vStorageIndexItemWithAssetRepository;
+        this.storageService = storageService;
     }
 
     @Nonnull
@@ -99,8 +121,9 @@ public class PdfActionNodeV1 implements ProcessNodeDefinition {
     @Override
     @JsonIgnore
     public ConfigLayoutElement getConfigurationLayout(@Nonnull ProcessNodeDefinitionContextConfig context) throws ResponseException {
+        ConfigLayoutElement layout;
         try {
-            return ElementPOJOMapper
+            layout = ElementPOJOMapper
                     .createFromPOJO(PdfActionNodeConfig.class);
         } catch (ElementDataConversionException e) {
             throw ResponseException.internalServerError(
@@ -108,6 +131,50 @@ public class PdfActionNodeV1 implements ProcessNodeDefinition {
                     e.getMessage()
             );
         }
+
+        layout
+                .findChild(PdfActionNodeConfig.CONTENT_HTML_SOURCE_FIELD_ID, RadioInputElement.class)
+                .ifPresent(element -> element.setOptions(List.of(
+                        RadioInputElementOption.of(PdfActionNodeConfig.CONTENT_HTML_SOURCE_FIELD_OPTION_CODE, "Manuelle Eingabe (HTML)"),
+                        RadioInputElementOption.of(PdfActionNodeConfig.CONTENT_HTML_SOURCE_FIELD_OPTION_ASSET_KEY, "PDF-Vorlage (Dateien & Medien)")
+                )));
+
+        layout
+                .findChild(PdfActionNodeConfig.CONTENT_HTML_CODE_FIELD_ID, CodeInputElement.class)
+                .ifPresent(element -> {
+                    element.setVisibility(ElementVisibilityFunctions.of(
+                                    NoCodeExpression.of(
+                                            NoCodeEqualsOperator.OPERATOR_ID,
+                                            NoCodeReference.of(PdfActionNodeConfig.CONTENT_HTML_SOURCE_FIELD_ID),
+                                            NoCodeStaticValue.of(PdfActionNodeConfig.CONTENT_HTML_SOURCE_FIELD_OPTION_CODE)
+                                    )
+                            )
+                            .recalculateReferencedIds());
+                });
+
+        layout
+                .findChild(PdfActionNodeConfig.CONTENT_HTML_ASSET_KEY_FIELD_ID, SelectInputElement.class)
+                .ifPresent(element -> {
+                    element.setVisibility(ElementVisibilityFunctions.of(
+                                    NoCodeExpression.of(
+                                            NoCodeEqualsOperator.OPERATOR_ID,
+                                            NoCodeReference.of(PdfActionNodeConfig.CONTENT_HTML_SOURCE_FIELD_ID),
+                                            NoCodeStaticValue.of(PdfActionNodeConfig.CONTENT_HTML_SOURCE_FIELD_OPTION_ASSET_KEY)
+                                    )
+                            )
+                            .recalculateReferencedIds());
+
+                    element.setOptions(vStorageIndexItemWithAssetRepository
+                            .findAllByMimeType("text/html")
+                            .stream()
+                            .map((ass) -> RadioInputElementOption.of(
+                                    ass.getAssetKey().toString(),
+                                    ass.getPathFromRoot()
+                            ))
+                            .toList());
+                });
+
+        return layout;
     }
 
     @Nonnull
@@ -184,31 +251,21 @@ public class PdfActionNodeV1 implements ProcessNodeDefinition {
             fileName += ".pdf";
         }
 
-        var contentHtml = processDataService
-                .interpolate(context.getProcessData(), configuration.contentHtml);
+        var contentHtml = resolveContentHtml(context, configuration);
         if (StringUtils.isNullOrEmpty(contentHtml)) {
             throw new ProcessNodeExecutionExceptionMissingValue(
                     "Der HTML-Inhalt fuer das PDF wurde nicht angegeben."
             );
         }
 
-        var headerHtml = processDataService
-                .interpolate(context.getProcessData(), configuration.headerHtml);
-        var footerHtml = processDataService
-                .interpolate(context.getProcessData(), configuration.footerHtml);
-        var paperWidth = processDataService
-                .interpolate(context.getProcessData(), configuration.paperWidth);
-        var paperHeight = processDataService
-                .interpolate(context.getProcessData(), configuration.paperHeight);
-
         byte[] pdfBytes;
         try {
             pdfBytes = pdfService.generatePdfFromHtml(
                     contentHtml,
-                    headerHtml,
-                    footerHtml,
-                    paperWidth,
-                    paperHeight
+                    "",
+                    "",
+                    "210mm",
+                    "297mm"
             );
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
@@ -259,14 +316,89 @@ public class PdfActionNodeV1 implements ProcessNodeDefinition {
                 .setNodeData(metadata);
     }
 
+    @Nonnull
+    private String resolveContentHtml(@Nonnull ProcessNodeExecutionContextInit context,
+                                      @Nonnull PdfActionNodeConfig configuration) throws ProcessNodeExecutionException {
+        var contentSource = configuration.contentHtmlSource;
+        if (StringUtils.isNullOrEmpty(contentSource)) {
+            contentSource = PdfActionNodeConfig.CONTENT_HTML_SOURCE_FIELD_OPTION_CODE;
+        }
+
+        if (PdfActionNodeConfig.CONTENT_HTML_SOURCE_FIELD_OPTION_CODE.equals(contentSource)) {
+            return processDataService
+                    .interpolate(context.getProcessData(), configuration.contentHtml);
+        }
+
+        if (PdfActionNodeConfig.CONTENT_HTML_SOURCE_FIELD_OPTION_ASSET_KEY.equals(contentSource)) {
+            var assetKeyStr = processDataService
+                    .interpolate(context.getProcessData(), configuration.contentHtmlAssetKey);
+            if (StringUtils.isNullOrEmpty(assetKeyStr)) {
+                throw new ProcessNodeExecutionExceptionMissingValue(
+                        "Der Asset-Schluessel fuer die PDF-Vorlage wurde nicht angegeben."
+                );
+            }
+
+            UUID assetKey;
+            try {
+                assetKey = UUID.fromString(assetKeyStr.trim());
+            } catch (IllegalArgumentException e) {
+                throw new ProcessNodeExecutionExceptionInvalidConfiguration(
+                        e,
+                        "Der Asset-Schluessel fuer die PDF-Vorlage ist ungueltig: %s",
+                        assetKeyStr
+                );
+            }
+
+            final var asset = retrieveAsset(assetKey);
+            return loadAssetContentAsString(asset.getStorageProviderId(), asset.getStoragePathFromRoot());
+        }
+
+        throw new ProcessNodeExecutionExceptionInvalidConfiguration(
+                "Die HTML-Quellkonfiguration %s ist unbekannt.",
+                StringUtils.quote(contentSource)
+        );
+    }
+
+    @Nonnull
+    private de.aivot.GoverBackend.asset.entities.AssetEntity retrieveAsset(@Nonnull UUID assetKey) throws ProcessNodeExecutionException {
+        try {
+            return assetService
+                    .retrieve(assetKey)
+                    .orElseThrow(() -> new ProcessNodeExecutionExceptionMissingValue(
+                            "Die PDF-Vorlage mit dem Schluessel %s wurde nicht gefunden.",
+                            assetKey.toString()
+                    ));
+        } catch (ResponseException e) {
+            throw new ProcessNodeExecutionExceptionUnknown(
+                    e,
+                    "Die PDF-Vorlage konnte nicht geladen werden: %s",
+                    e.getMessage()
+            );
+        }
+    }
+
+    @Nonnull
+    private String loadAssetContentAsString(@Nonnull Integer storageProviderId,
+                                            @Nonnull String storagePathFromRoot) throws ProcessNodeExecutionException {
+        try (var content = storageService.getDocumentContent(storageProviderId, storagePathFromRoot)) {
+            return new String(content.readAllBytes(), StandardCharsets.UTF_8);
+        } catch (ResponseException | IOException e) {
+            throw new ProcessNodeExecutionExceptionUnknown(
+                    e,
+                    "Der Inhalt der PDF-Vorlage konnte nicht geladen werden: %s",
+                    e.getMessage()
+            );
+        }
+    }
+
     @LayoutElementPOJOBinding(id = NODE_KEY, type = ElementType.ConfigLayout)
     public static class PdfActionNodeConfig {
         public static final String FILE_NAME_FIELD_ID = "file_name";
-        public static final String CONTENT_HTML_FIELD_ID = "content_html";
-        public static final String HEADER_HTML_FIELD_ID = "header_html";
-        public static final String FOOTER_HTML_FIELD_ID = "footer_html";
-        public static final String PAPER_WIDTH_FIELD_ID = "paper_width";
-        public static final String PAPER_HEIGHT_FIELD_ID = "paper_height";
+        public static final String CONTENT_HTML_SOURCE_FIELD_ID = "content_html_source";
+        public static final String CONTENT_HTML_SOURCE_FIELD_OPTION_CODE = "contentHtml";
+        public static final String CONTENT_HTML_SOURCE_FIELD_OPTION_ASSET_KEY = "contentHtmlAssetKey";
+        public static final String CONTENT_HTML_CODE_FIELD_ID = "content_html_code";
+        public static final String CONTENT_HTML_ASSET_KEY_FIELD_ID = "content_html_asset_key";
 
         @InputElementPOJOBinding(id = FILE_NAME_FIELD_ID, type = ElementType.Text, properties = {
                 @ElementPOJOBindingProperty(key = "label", strValue = "Dateiname"),
@@ -275,7 +407,14 @@ public class PdfActionNodeV1 implements ProcessNodeDefinition {
         })
         public String fileName;
 
-        @InputElementPOJOBinding(id = CONTENT_HTML_FIELD_ID, type = ElementType.CodeInput, properties = {
+        @InputElementPOJOBinding(id = CONTENT_HTML_SOURCE_FIELD_ID, type = ElementType.Radio, properties = {
+                @ElementPOJOBindingProperty(key = "label", strValue = "Quelle für die PDF-Vorlage"),
+                @ElementPOJOBindingProperty(key = "hint", strValue = "Wählen Sie die Quelle für den HTML-Inhalt der PDF-Seiten."),
+                @ElementPOJOBindingProperty(key = "required", boolValue = true),
+        })
+        public String contentHtmlSource;
+
+        @InputElementPOJOBinding(id = CONTENT_HTML_CODE_FIELD_ID, type = ElementType.CodeInput, properties = {
                 @ElementPOJOBindingProperty(key = "label", strValue = "HTML-Inhalt"),
                 @ElementPOJOBindingProperty(key = "hint", strValue = "HTML-Template fuer die PDF-Seiten."),
                 @ElementPOJOBindingProperty(key = "required", boolValue = true),
@@ -283,34 +422,11 @@ public class PdfActionNodeV1 implements ProcessNodeDefinition {
         })
         public String contentHtml;
 
-        @InputElementPOJOBinding(id = HEADER_HTML_FIELD_ID, type = ElementType.CodeInput, properties = {
-                @ElementPOJOBindingProperty(key = "label", strValue = "Header HTML (optional)"),
-                @ElementPOJOBindingProperty(key = "hint", strValue = "Optionales HTML fuer den Seitenkopf."),
-                @ElementPOJOBindingProperty(key = "required", boolValue = false),
-                @ElementPOJOBindingProperty(key = "language", strValue = "html")
+        @InputElementPOJOBinding(id = CONTENT_HTML_ASSET_KEY_FIELD_ID, type = ElementType.Select, properties = {
+                @ElementPOJOBindingProperty(key = "label", strValue = "PDF-Vorlage"),
+                @ElementPOJOBindingProperty(key = "hint", strValue = "Wählen Sie eine zuvor hochgeladene PDF-Vorlage aus."),
+                @ElementPOJOBindingProperty(key = "required", boolValue = true),
         })
-        public String headerHtml;
-
-        @InputElementPOJOBinding(id = FOOTER_HTML_FIELD_ID, type = ElementType.CodeInput, properties = {
-                @ElementPOJOBindingProperty(key = "label", strValue = "Footer HTML (optional)"),
-                @ElementPOJOBindingProperty(key = "hint", strValue = "Optionales HTML fuer die Seitenfusszeile."),
-                @ElementPOJOBindingProperty(key = "required", boolValue = false),
-                @ElementPOJOBindingProperty(key = "language", strValue = "html")
-        })
-        public String footerHtml;
-
-        @InputElementPOJOBinding(id = PAPER_WIDTH_FIELD_ID, type = ElementType.Text, properties = {
-                @ElementPOJOBindingProperty(key = "label", strValue = "Papierbreite"),
-                @ElementPOJOBindingProperty(key = "hint", strValue = "Optional, Standard ist 210mm."),
-                @ElementPOJOBindingProperty(key = "required", boolValue = false)
-        })
-        public String paperWidth;
-
-        @InputElementPOJOBinding(id = PAPER_HEIGHT_FIELD_ID, type = ElementType.Text, properties = {
-                @ElementPOJOBindingProperty(key = "label", strValue = "Papierhoehe"),
-                @ElementPOJOBindingProperty(key = "hint", strValue = "Optional, Standard ist 297mm."),
-                @ElementPOJOBindingProperty(key = "required", boolValue = false)
-        })
-        public String paperHeight;
+        public String contentHtmlAssetKey;
     }
 }
