0618d299f390669ab6cf13d31517dea5abdd7e6d
Daniel Kratz <daniel.kratz@aivot.de>
2026-03-07 13:30:41 +0100

    feat: implement No-Code Action Node with variable handling and expression evaluation

--- app/src/modules/process/data/known-provider-icons.ts
+++ app/src/modules/process/data/known-provider-icons.ts
@@ -4,10 +4,12 @@ import FactCheck from "@aivot/mui-material-symbols-400-outlined/dist/fact-check/
 import Webhook from "@aivot/mui-material-symbols-400-outlined/dist/webhook/Webhook";
 import {SvgIconProps} from "@mui/material";
 import {FC} from "react";
+import RegularExpression from '@aivot/mui-material-symbols-400-outlined/dist/regular-expression/RegularExpression';
 
 export const KnownProviderIcons: Record<string, FC<SvgIconProps>> = {
     'if': AltRoute,
     'webhook': Webhook,
     'js': Code,
+    'no-code': RegularExpression,
     'check-and-update': FactCheck,
 };
