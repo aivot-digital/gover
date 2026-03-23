8f8c6eb54c8d302a2aecccd01eeaf7ef661666cf
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-03-23 09:27:53 +0100

    feat: add method to find ProcessInstances by status and keepUntil date

--- src/main/java/de/aivot/GoverBackend/process/repositories/ProcessInstanceRepository.java
+++ src/main/java/de/aivot/GoverBackend/process/repositories/ProcessInstanceRepository.java
@@ -5,10 +5,14 @@ import de.aivot.GoverBackend.process.enums.ProcessInstanceStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface ProcessInstanceRepository extends JpaRepository<ProcessInstanceEntity, Long>, JpaSpecificationExecutor<ProcessInstanceEntity> {
     List<ProcessInstanceEntity> findAllByStatus(ProcessInstanceStatus status);
+
+    List<ProcessInstanceEntity> findAllByStatusAndKeepUntilLessThanEqual(ProcessInstanceStatus status,
+                                                                         LocalDateTime keepUntil);
 
     List<ProcessInstanceEntity> findAllByCreatedForTestClaimId(Integer createdForTestClaimId);
 }
