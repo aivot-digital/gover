8fb69275015cae44e635d91f0d8b560b0e7276f2
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-04-08 14:25:05 +0200

    feat(shell): comment out destinations menu item in shell drawer

--- app/src/shells/staff/components/shell-drawer.tsx
+++ app/src/shells/staff/components/shell-drawer.tsx
@@ -166,7 +166,7 @@ const BaseDrawerGroups: DrawerGroup[] = [
                             {icon: ModuleIcons.identity, label: 'Identitätsanbieter', to: '/identity-providers'},
                             {icon: ModuleIcons.payment, label: 'Zahlungsanbieter', to: '/payment-providers'},
                             {icon: ModuleIcons.storage, label: 'Speicheranbieter', to: '/storage-providers'},
-                            {icon: ModuleIcons.destinations, label: 'Schnittstellen', to: '/destinations'},
+                            // {icon: ModuleIcons.destinations, label: 'Schnittstellen', to: '/destinations'},
                         ],
                     },
                     {icon: ModuleIcons.extensions, label: 'Erweiterungen', to: '/settings/extensions'},
