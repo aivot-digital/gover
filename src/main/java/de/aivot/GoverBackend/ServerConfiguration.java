d4866b1e07542b9e1136eea1d72a79be33f2166b
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-04-09 18:19:40 +0200

    feat(process): enhance content negotiation and XML message conversion in server configuration

--- src/main/java/de/aivot/GoverBackend/ServerConfiguration.java
+++ src/main/java/de/aivot/GoverBackend/ServerConfiguration.java
@@ -1,20 +1,22 @@
 package de.aivot.GoverBackend;
 
+import com.fasterxml.jackson.dataformat.xml.JacksonXmlModule;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.fasterxml.jackson.dataformat.xml.deser.FromXmlParser;
 import de.aivot.GoverBackend.system.properties.CORSProperties;
 import jakarta.annotation.Nonnull;
-import org.apache.tomcat.util.buf.EncodedSolidusHandling;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.amqp.support.converter.SimpleMessageConverter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.actuate.web.exchanges.InMemoryHttpExchangeRepository;
 import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
-import org.springframework.boot.web.embedded.tomcat.TomcatConnectorCustomizer;
-import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
-import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.xml.MappingJackson2XmlHttpMessageConverter;
+import org.springframework.web.accept.FixedContentNegotiationStrategy;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -22,13 +24,16 @@ import java.util.List;
 
 @Configuration
 public class ServerConfiguration implements WebMvcConfigurer {
-    private static final Logger logger = LoggerFactory.getLogger(ServerConfiguration.class);
-
     private final CORSProperties corsProperties;
 
     @Autowired
     public ServerConfiguration(CORSProperties corsProperties) {
         this.corsProperties = corsProperties;
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.strategies(List.of(new FixedContentNegotiationStrategy(MediaType.APPLICATION_JSON)));
     }
 
     @Bean
@@ -69,5 +74,21 @@ public class ServerConfiguration implements WebMvcConfigurer {
         SimpleMessageConverter converter = new SimpleMessageConverter();
         converter.setAllowedListPatterns(List.of("de.aivot.GoverBackend.process.workers.*"));
         return converter;
+    }
+
+    @Override
+    public void extendMessageConverters(List<HttpMessageConverter<?>> converters) {
+        // XmlMapper extends ObjectMapper. Exposing it as a bean would replace Boot's default JSON mapper.
+        converters.removeIf(MappingJackson2XmlHttpMessageConverter.class::isInstance);
+        converters.add(new MappingJackson2XmlHttpMessageConverter(createXmlMapper()));
+    }
+
+    private static XmlMapper createXmlMapper() {
+        JacksonXmlModule module = new JacksonXmlModule();
+        module.setXMLTextElementName("#text");
+
+        XmlMapper mapper = new XmlMapper(module);
+        mapper.configure(FromXmlParser.Feature.EMPTY_ELEMENT_AS_NULL, true);
+        return mapper;
     }
 }
