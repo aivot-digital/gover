51395d3a9d9100d05e02e54a24fdd921cdc15311
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-04-17 15:40:58 +0200

    feat(data-objects): update area fields to use numeric values instead of strings

--- dev/examples/vesoz/07-data-objects.sql
+++ dev/examples/vesoz/07-data-objects.sql
@@ -203,12 +203,12 @@ values ('leistungserbringer',
          {
            "bezeichner": "Küche",
            "stockwerk": "EG",
-           "flaeche_qm": "28"
+           "flaeche_qm": 28
          },
          {
            "bezeichner": "Gemeinschaftsraum",
            "stockwerk": "1. OG",
-           "flaeche_qm": "13"
+           "flaeche_qm": 13
          }
        ]
      }'),
@@ -225,12 +225,12 @@ values ('leistungserbringer',
             {
               "bezeichner": "Werkraum",
               "stockwerk": "EG",
-              "flaeche_qm": "59"
+              "flaeche_qm": 59
             },
             {
               "bezeichner": "Waschküche",
               "stockwerk": "EG",
-              "flaeche_qm": "9"
+              "flaeche_qm": 9
             }
           ]
         }')
