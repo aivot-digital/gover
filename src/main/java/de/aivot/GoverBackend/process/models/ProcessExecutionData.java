c1eab266302ee0339e531b61a61ae1f88c4b1c3e
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-04-17 10:19:51 +0200

    feat(process): add patchWithElementData method to ProcessExecutionData for enhanced data integration

--- src/main/java/de/aivot/GoverBackend/process/models/ProcessExecutionData.java
+++ src/main/java/de/aivot/GoverBackend/process/models/ProcessExecutionData.java
@@ -1,13 +1,16 @@
 package de.aivot.GoverBackend.process.models;
 
+import de.aivot.GoverBackend.elements.models.EffectiveElementValues;
+import de.aivot.GoverBackend.elements.models.elements.BaseElement;
 import de.aivot.GoverBackend.process.entities.ProcessNodeEntity;
+import de.aivot.GoverBackend.submission.services.ElementDataTransformService;
+import de.aivot.GoverBackend.utils.MapUtils;
 
 import java.util.HashMap;
 import java.util.Map;
 
 /**
- * This is the data structure used to hold process instance data during execution.
- * It extends HashMap to allow dynamic storage of key-value pairs.
+ * This is the data structure used to hold process instance data during execution. It extends HashMap to allow dynamic storage of key-value pairs.
  * <p>
  * The following keys are always set:
  * <ul>
@@ -17,9 +20,15 @@ import java.util.Map;
  * </ul>
  */
 public class ProcessExecutionData extends HashMap<String, Object> {
-    private static final String PROCESS_DATA_KEY = "$";
-    private static final String PROCESS_METADATA_KEY = "$$";
-    private static final String NODE_RESULTS_KEY = "_";
+    public static final String PROCESS_DATA_KEY = "$";
+    public static final String PROCESS_METADATA_KEY = "$$";
+    public static final String NODE_RESULTS_KEY = "_";
+
+    public static final String[] PROCESS_EXEC_DATA_KEYS = {
+            PROCESS_DATA_KEY,
+            PROCESS_METADATA_KEY,
+            NODE_RESULTS_KEY
+    };
 
     public static ProcessExecutionData of(Map<String, Object> data) {
         ProcessExecutionData executionData = new ProcessExecutionData();
@@ -40,6 +49,29 @@ public class ProcessExecutionData extends HashMap<String, Object> {
         dataMap.put(key, value);
 
         return this;
+    }
+
+    public ProcessExecutionData patchWithElementData(ElementDataTransformService elementDataTransformService,
+                                                     BaseElement baseElement,
+                                                     EffectiveElementValues effectiveElementValues) {
+        var clone = new ProcessExecutionData();
+        clone.putAll(MapUtils.deepCopy(this));
+
+        @SuppressWarnings("unchecked")
+        Map<String, Object> processData = (Map<String, Object>) clone.get(PROCESS_DATA_KEY);
+        if (processData == null) {
+            processData = new HashMap<>();
+        }
+
+        var d = elementDataTransformService
+                .buildPayload(
+                        baseElement,
+                        effectiveElementValues,
+                        processData
+                );
+        clone.put(PROCESS_DATA_KEY, d);
+
+        return clone;
     }
 
     public ProcessExecutionData addProcessMetadata(Object processMetadata) {
