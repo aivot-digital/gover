9a81a54d33f69b149dde409041fda380338ae2ed
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-04-17 11:16:22 +0200

    feat(editor): enhance NoCodeOperandEditorProcessDataReference layout with improved structure and actions

--- app/src/modules/nocode/components/no-code-operand-editor-process-data-reference.tsx
+++ app/src/modules/nocode/components/no-code-operand-editor-process-data-reference.tsx
@@ -1,7 +1,7 @@
 import Delete from '@aivot/mui-material-symbols-400-outlined/dist/delete/Delete';
 import Functions from '@aivot/mui-material-symbols-400-outlined/dist/functions/Functions';
 import DatabaseSearch from '@aivot/mui-material-symbols-400-outlined/dist/database-search/DatabaseSearch';
-import {Box} from '@mui/material';
+import {Box, Grid, Stack} from '@mui/material';
 import {
     isNoCodeNodeDataReference,
     NoCodeInstanceDataReference,
@@ -10,6 +10,7 @@ import {
     isNoCodeProcessDataReference,
 } from '../../../models/functions/no-code-expression';
 import {TextFieldComponent} from '../../../components/text-field/text-field-component';
+import {Actions} from '../../../components/actions/actions';
 
 interface NoCodeOperandEditorProcessDataReferenceProps {
     label: string;
@@ -33,57 +34,95 @@ export function NoCodeOperandEditorProcessDataReference(props: NoCodeOperandEdit
     const sourceLabel = isProcessDataReference
         ? 'Vorgangsdaten ($)'
         : isNodeDataReference
-            ? 'Elementdaten (_)'
+            ? 'Pfad in den Elementdaten'
             : 'Geschützte Vorgangsdaten ($$)';
 
     const startIcon = isProcessDataReference
         ? '$.'
         : isNodeDataReference
-            ? undefined
+            ? `_.${value.nodeDataKey}.`
             : '$$.';
 
     return (
-        <Box sx={{display: 'flex', flexDirection: 'column', gap: 1}}>
+        <Grid
+            container
+            spacing={2}
+        >
             {
                 isNodeDataReference &&
-                <TextFieldComponent
-                    label="Datenschlüssel"
-                    hint="Der Datenschlüssel des Prozessknotens, dessen Ergebnis gelesen werden soll."
-                    value={value.nodeDataKey ?? undefined}
-                    onChange={(nodeDataKey) => onChange({
-                        ...value,
-                        nodeDataKey: nodeDataKey ?? undefined,
-                    })}
-                    muiPassTroughProps={{margin: 'none'}}
-                    startIcon="_."
-                />
+                <Grid size={4}>
+                    <TextFieldComponent
+                        label="Datenschlüssel des Prozesselementes"
+                        hint="Der Datenschlüssel des Prozesselementes, aus dessen Ergebnis gelesen werden soll."
+                        value={value.nodeDataKey ?? undefined}
+                        onChange={(nodeDataKey) => onChange({
+                            ...value,
+                            nodeDataKey: nodeDataKey ?? undefined,
+                        })}
+                        muiPassTroughProps={{margin: 'none'}}
+                        startIcon="_."
+                    />
+                </Grid>
             }
 
-            <TextFieldComponent
-                label={`${label ?? ''} — (${sourceLabel})`}
-                hint={hint}
-                value={value.path ?? undefined}
-                onChange={(path) => {
-                    onChange({
-                        ...value,
-                        path: path ?? undefined,
-                    });
-                }}
-                startIcon={startIcon}
-                endAction={[
+            <Grid size={isNodeDataReference ? 8 : 12}>
+                <Stack
+                    direction="row"
+                    alignItems="flex-start"
+                >
+                    <TextFieldComponent
+                        label={`${label ?? ''} — (${sourceLabel})`}
+                        hint={hint}
+                        value={value.path ?? undefined}
+                        onChange={(path) => {
+                            onChange({
+                                ...value,
+                                path: path ?? undefined,
+                            });
+                        }}
+                        startIcon={startIcon}
+                        endAction={isNodeDataReference ? undefined : [
+                            {
+                                icon: <Delete/>,
+                                tooltip: 'Diesen Vorgangsdaten-Verweis löschen',
+                                onClick: () => onChange(undefined),
+                            },
+                            {
+                                tooltip: 'Diesen Verweis mit einem Ausdruck verknüpfen',
+                                icon: <Functions/>,
+                                onClick: onAddEnclosingExpression,
+                            },
+                        ]}
+                        muiPassTroughProps={{margin: 'none'}}
+                    />
+
                     {
-                        icon: <Delete />,
-                        tooltip: 'Diesen Vorgangsdaten-Verweis löschen',
-                        onClick: () => onChange(undefined),
-                    },
-                    {
-                        tooltip: 'Diesen Verweis mit einem Ausdruck verknüpfen',
-                        icon: <Functions />,
-                        onClick: onAddEnclosingExpression,
-                    },
-                ]}
-                muiPassTroughProps={{margin: 'none'}}
-            />
-        </Box>
+                        isNodeDataReference &&
+                        <Actions
+                            size="small"
+                            dense={true}
+                            color="inherit"
+                            actions={[
+                                {
+                                    icon: <Delete/>,
+                                    tooltip: 'Diesen Vorgangsdaten-Verweis löschen',
+                                    onClick: () => onChange(undefined),
+                                },
+                                {
+                                    tooltip: 'Diesen Verweis mit einem Ausdruck verknüpfen',
+                                    icon: <Functions/>,
+                                    onClick: onAddEnclosingExpression,
+                                },
+                            ]}
+                            sx={{
+                                mt: 1.5,
+                                ml: 1,
+                                opacity: 0.66,
+                            }}
+                        />
+                    }
+                </Stack>
+            </Grid>
+        </Grid>
     );
 }
