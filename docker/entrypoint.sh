cc14294b81281a4d12220250abe66dd7a376f2be
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-04-24 14:07:55 +0200

    refactor(entrypoint): simplify app configuration setup in entrypoint script

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -17,29 +17,7 @@ if [ "$1" = "serve" ]; then
     -Dloader.path=/app/plugins/ \
     org.springframework.boot.loader.launch.PropertiesLauncher
 else
-  cat > /app/app-config.js <<EOF
-window.AppConfig = {
-    oidc: {
-        realm: '$GOVER_KEYCLOAK_OIDC_REALM',
-        client: '$GOVER_KEYCLOAK_OIDC_FRONTEND_CLIENT_ID',
-        hostname: '$GOVER_KEYCLOAK_OIDC_HOSTNAME',
-        idp_hint: '$GOVER_KEYCLOAK_OIDC_IDP_HINT',
-    },
-    api: {
-        hostname: '$GOVER_HOSTNAME',
-    },
-    registry: {
-        url: '$GOVER_TEMPLATE_REGISTRY_URL',
-    },
-    sentry: {
-        dsn: '$GOVER_SENTRY_WEB_APP',
-    },
-};
-EOF
-
   echo "app" > /app/runtime-mode
-  cp /app/app-config.js /app/www/app-config.js
-  cp /app/app-config.js /app/www/staff/app-config.js
 
   echo "Waiting for the API to be available at ${GOVER_HOSTNAME}/api/actuator/health…"
   until curl --output /dev/null --silent --head --fail "${GOVER_HOSTNAME}/api/actuator/health"; do
