8e9c43282fc051c90a48cf53eed95ee183168f77
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-05-18 09:59:09 +0200

    refactor(elements): enhance isAnyElementWithChildren function to validate element type

--- app/src/models/elements/any-element-with-children.ts
+++ app/src/models/elements/any-element-with-children.ts
@@ -2,6 +2,7 @@ import {type FormLayoutElement} from './form-layout-element';
 import {type StepElement} from './steps/step-element';
 import {type AnyLayoutElement} from './form/layout/any-layout-element';
 import {IntroductionStepElement} from './steps/introduction-step-element';
+import {ElementType} from '../../data/element-type/element-type';
 
 export type AnyElementWithChildren =
     FormLayoutElement |
@@ -10,5 +11,15 @@ export type AnyElementWithChildren =
     AnyLayoutElement;
 
 export function isAnyElementWithChildren(obj: any): obj is AnyElementWithChildren {
-    return obj != null && 'children' in obj && Array.isArray(obj.children);
+    return obj != null &&
+        'type' in obj &&
+        [
+            ElementType.FormLayout,
+            ElementType.Step,
+            ElementType.IntroductionStep,
+            ElementType.GroupLayout,
+            ElementType.ReplicatingContainer,
+            ElementType.SummaryLayout,
+            ElementType.StepperLayout,
+        ].includes(obj.type);
 }
