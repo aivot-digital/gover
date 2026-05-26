c7d85afcdade49df44a3d2ddf3014efcc8fcd821
Daniel Kratz <daniel.kratz@aivot.de>
2026-05-26 17:54:56 +0200

    fix(form): disable Gover Marktplatz tab since the integration is currently not working
    #597

--- app/src/dialogs/add-element-dialog/add-element-dialog.tsx
+++ app/src/dialogs/add-element-dialog/add-element-dialog.tsx
@@ -44,7 +44,7 @@ export function AddElementDialog(props: AddElementDialogProps) {
             tabs={[
                 {label: 'Elemente', value: 0},
                 {label: 'Vorlagen', value: 1, hidden: props.hidePresets === true, disabled: true},
-                {label: 'Gover Marktplatz', value: 2, hidden: props.hideGoverStore === true},
+                {label: 'Gover Marktplatz', value: 2, hidden: props.hideGoverStore === true, disabled: true},
             ]}
             activeTab={currentTab}
             onTabChange={(value) => {
