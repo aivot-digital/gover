a6bcfd8d006d74042f3a6caf165ccc247ca8e263
Daniel Kratz <daniel.kratz@aivot.de>
2026-05-27 22:24:31 +0200

    feat(submit-component-editor): restructure layout with Grid

--- app/src/components/submit/submit.component.editor.tsx
+++ app/src/components/submit/submit.component.editor.tsx
@@ -5,72 +5,114 @@ import {TextFieldComponent} from '../text-field/text-field-component';
 import {StringListInput} from '../string-list-input/string-list-input';
 import {RichTextInputComponent} from '../rich-text-input-component/rich-text-input-component';
 import {CheckboxFieldComponent} from '../checkbox-field/checkbox-field-component';
+import { Grid } from '@mui/material';
 
 export function SubmitComponentEditor(props: BaseEditorProps<SubmitStepElement>) {
     return (
         <>
-            <RichTextInputComponent
-                value={props.element.textPreSubmit ?? ''}
-                label="Text vor dem Absenden des Antrages"
-                onChange={(val) => {
-                    props.onPatch({
-                        textPreSubmit: val ?? undefined,
-                    });
-                }}
-                disabled={!props.editable}
-            />
+            <Grid
+                container
+                columnSpacing={4}
+                sx={{mt: 2}}
+            >
+                <Grid
+                    size={{
+                        xs: 12,
+                        lg: 6,
+                    }}
+                    sx={{mb: 2}}
+                >
+                    <RichTextInputComponent
+                        value={props.element.textPreSubmit ?? ''}
+                        label="Text vor dem Absenden des Antrages"
+                        onChange={(val) => {
+                            props.onPatch({
+                                textPreSubmit: val ?? undefined,
+                            });
+                        }}
+                        disabled={!props.editable}
+                    />
+                </Grid>
+                <Grid
+                    size={{
+                        xs: 12,
+                        lg: 6,
+                    }}
+                    sx={{mb: 2}}
+                >
+                    <RichTextInputComponent
+                        value={props.element.textPostSubmit ?? ''}
+                        label="Text nach dem Absenden des Antrages"
+                        onChange={(val) => {
+                            props.onPatch({
+                                textPostSubmit: val ?? undefined,
+                            });
+                        }}
+                        disabled={!props.editable}
+                    />
+                </Grid>
 
-            <RichTextInputComponent
-                value={props.element.textPostSubmit ?? ''}
-                label="Text nach dem Absenden des Antrages"
-                onChange={(val) => {
-                    props.onPatch({
-                        textPostSubmit: val ?? undefined,
-                    });
-                }}
-                disabled={!props.editable}
-            />
-
-            <TextFieldComponent
-                value={props.element.textProcessingTime ?? ''}
-                label="Bearbeitungszeit des Antrages"
-                hint="Ungefähre Bearbeitungszeit der zuständigen und/oder bewirtschaftenden Stelle."
-                multiline
-                rows={4}
-                onChange={(val) => {
-                    props.onPatch({
-                        textProcessingTime: val,
-                    });
-                }}
-                disabled={!props.editable}
-            />
-
-            <StringListInput
-                value={props.element.documentsToReceive ?? undefined}
-                label="Dokumente die antragstellende Personen erhalten"
-                onChange={(val) => {
-                    props.onPatch({
-                        documentsToReceive: val,
-                    });
-                }}
-                hint="Durch die zuständige und/oder bewirtschaftende Stelle auszustellende Dokumente. Bitte geben Sie pro Zeile ein Dokument an."
-                disabled={!props.editable}
-                allowEmpty
-                addLabel="Dokument hinzufügen"
-                noItemsHint="Keine Dokumente angegeben"
-            />
-
-            <CheckboxFieldComponent
-                label="Konfetti nach dem Absenden deaktivieren"
-                value={props.element.disableConfetti ?? false}
-                onChange={(val) => {
-                    props.onPatch({
-                        disableConfetti: val,
-                    });
-                }}
-                disabled={!props.editable}
-                hint="Wenn aktiviert, wird nach erfolgreicher Antragseinreichung keine Konfetti-Animation angezeigt."
-            />
+                <Grid
+                    size={{
+                        xs: 12,
+                        lg: 6,
+                    }}
+                    sx={{mb: 2}}
+                >
+                    <TextFieldComponent
+                        value={props.element.textProcessingTime ?? ''}
+                        label="Bearbeitungszeit des Antrages"
+                        hint="Ungefähre Bearbeitungszeit der zuständigen und/oder bewirtschaftenden Stelle."
+                        multiline
+                        rows={4}
+                        onChange={(val) => {
+                            props.onPatch({
+                                textProcessingTime: val,
+                            });
+                        }}
+                        disabled={!props.editable}
+                    />
+                </Grid>
+                <Grid
+                    size={{
+                        xs: 12,
+                    }}
+                    sx={{mb: 2}}
+                >
+                    <StringListInput
+                        value={props.element.documentsToReceive ?? undefined}
+                        label="Dokumente die antragstellende Personen erhalten"
+                        onChange={(val) => {
+                            props.onPatch({
+                                documentsToReceive: val,
+                            });
+                        }}
+                        hint="Durch die zuständige und/oder bewirtschaftende Stelle auszustellende Dokumente. Bitte geben Sie pro Zeile ein Dokument an."
+                        disabled={!props.editable}
+                        allowEmpty
+                        addLabel="Dokument hinzufügen"
+                        noItemsHint="Keine Dokumente angegeben"
+                    />
+                </Grid>
+                <Grid
+                    size={{
+                        xs: 12,
+                    }}
+                    sx={{mb: 2}}
+                >
+                    <CheckboxFieldComponent
+                        label="Konfetti nach dem Absenden deaktivieren"
+                        value={props.element.disableConfetti ?? false}
+                        onChange={(val) => {
+                            props.onPatch({
+                                disableConfetti: val,
+                            });
+                        }}
+                        disabled={!props.editable}
+                        hint="Wenn aktiviert, wird nach erfolgreicher Antragseinreichung keine Konfetti-Animation angezeigt."
+                    />
+                </Grid>
+            </Grid>
         </>
     );
 }
