b0388a3f399ff090846576e9237b8374e530c94c
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-05-29 11:54:21 +0200

    feat(case-number-generator): implement case number generation and validation service

---
+++ app/src/modules/process/pages/details/process-instance-details-page.tsx
@@ -0,0 +1,16 @@
+import {Box} from '@mui/material';
+import {PageWrapper} from '../../../../components/page-wrapper/page-wrapper';
+
+export function ProcessInstanceDetailsPage() {
+    return (
+        <PageWrapper
+            title="Vorgang"
+            fullWidth={false}
+            fullHeight={true}
+        >
+            <Box>
+                Hier sind später die Details zu einem Vorgang zu finden.
+            </Box>
+        </PageWrapper>
+    );
+}
