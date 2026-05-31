5861ea44f97689d227c2cead36642a40bbaa5c8e
Daniel Kratz <daniel.kratz@aivot.de>
2026-05-31 16:02:00 +0200

    fix(actions): make icon prop optional in actions props definition

--- app/src/components/actions/actions-props.ts
+++ app/src/components/actions/actions-props.ts
@@ -10,7 +10,7 @@ interface ActionBase {
     tooltip?: string;
     disabledTooltip?: string;
     ariaLabel?: string;
-    icon: ReactNode;
+    icon?: ReactNode;
     iconPosition?: 'start' | 'end';
     disabled?: boolean;
     visible?: boolean;
