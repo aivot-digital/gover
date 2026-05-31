13c818f20c22edc3e08608c3af66667b547a1818
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-05-31 15:59:52 +0200

    [#565] feat(text-input): add copy flag to text field element

---
+++ app/src/utils/generate-element-with-default-values.spec.ts
@@ -0,0 +1,11 @@
+import {ElementType} from '../data/element-type/element-type';
+import {type TextFieldElement} from '../models/elements/form/input/text-field-element';
+import {generateElementWithDefaultValues} from './generate-element-with-default-values';
+
+describe('generateElementWithDefaultValues', () => {
+    it('should default text fields to not being copyable', () => {
+        const element = generateElementWithDefaultValues(ElementType.Text) as TextFieldElement;
+
+        expect(element.copyable).toBe(false);
+    });
+});
