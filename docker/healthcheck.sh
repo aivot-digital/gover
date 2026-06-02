92ca694d2e95a0a3643691ffcea429520cc928d8
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-06-02 17:11:58 +0200

    fix: fix app health check after app port change to 8080

--- docker/healthcheck.sh
+++ docker/healthcheck.sh
@@ -5,5 +5,5 @@ mode="$(cat /app/runtime-mode 2>/dev/null || echo app)"
 if [ "$mode" = "serve" ]; then
   curl --output /dev/null --silent --fail http://127.0.0.1:8080/api/actuator/health
 else
-  curl --output /dev/null --silent --fail http://127.0.0.1/
+  curl --output /dev/null --silent --fail http://127.0.0.1:8080/
 fi
