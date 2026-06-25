820f59423f07d5d3f8b768b7e5d34578b609d997
Daniel Kratz <daniel.kratz@aivot.de>
2026-06-25 11:36:09 +0200

    feat(process-node-editor): shorten success message for saved element

--- app/src/modules/process/pages/details/components/process-node-editor/process-node-editor.tsx
+++ app/src/modules/process/pages/details/components/process-node-editor/process-node-editor.tsx
@@ -278,7 +278,7 @@ export function ProcessNodeEditor(): ReactNode {
                 setOriginalNode(savedNode);
                 setEditedNode(savedNode);
 
-                dispatch(showSuccessSnackbar('Das Prozesselement wurde erfolgreich gespeichert.'));
+                dispatch(showSuccessSnackbar('Das Element wurde erfolgreich gespeichert.'));
 
                 return new ProcessNodeApiService()
                     .validate(editedNode.id);
