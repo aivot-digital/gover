367c80104dd0e8de50798b218b9b65e75f69ceff
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-07-27 12:47:29 +0200

    feat: enhance code list source reference handling for plugin key validation

--- backend/src/main/java/de/aivot/gover/backend/codeLists/services/CodeListStartupService.java
+++ backend/src/main/java/de/aivot/gover/backend/codeLists/services/CodeListStartupService.java
@@ -16,6 +16,7 @@ import org.springframework.context.ApplicationListener;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 
+import java.util.Arrays;
 import java.util.List;
 import java.util.Set;
 import java.util.stream.Collectors;
@@ -52,7 +53,15 @@ public class CodeListStartupService implements ApplicationListener<ApplicationRe
         }
 
         for (var codeList : codeLists) {
-            if (existingPluginKeys.contains(codeList.getSourceRef())) {
+            var codeListRefParts = codeList
+                    .getSourceRef()
+                    .split("\\.");
+
+            var pluginKey = String.join(".", Arrays
+                    .asList(codeListRefParts)
+                    .subList(0, codeListRefParts.length - 1));
+
+            if (existingPluginKeys.contains(pluginKey)) {
                 continue;
             }
 
