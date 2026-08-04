55d526c8035cf4cfba64879ec3400b8022e8514b
Daniel Kratz <daniel.kratz@aivot.de>
2026-08-04 20:09:56 +0200

    feat: replace alpha version notice with generic pre-release version notice dialog

--- app/src/data/storage-key.ts
+++ app/src/data/storage-key.ts
@@ -2,7 +2,7 @@ export enum StorageKey {
     AuthDataAccessToken = 'access_token',
     AuthDataRefreshToken = 'refresh_token',
 
-    AlphaVersionNoticeDismissed = 'alpha_version_notice_dismissed',
+    PreReleaseVersionNoticeDismissed = 'pre_release_version_notice_dismissed',
     IntroDismissed = 'intro_dismissed',
 
     SubmissionsIncludePaymentPending = 'submissions_include_payment_pending',
