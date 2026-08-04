2f71ce6987dd5c5b212524c018bba6975fa87ab7
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-08-04 17:28:15 +0200

    feat: enhance FormTriggerCostCalculationResponseV1 with new constructors and payment item model

--- backend/src/main/java/de/aivot/gover/backend/plugins/form/v1/nodes/FormTriggerCostCalculationResponseV1.java
+++ backend/src/main/java/de/aivot/gover/backend/plugins/form/v1/nodes/FormTriggerCostCalculationResponseV1.java
@@ -1,9 +1,31 @@
 package de.aivot.gover.backend.plugins.form.v1.nodes;
 
-import de.aivot.gover.backend.payment.models.PaymentItem;
+import de.aivot.gover.backend.payment.entities.PaymentProviderEntity;
+import de.aivot.gover.backend.payment.models.XBezahldienstePaymentItem;
+import de.aivot.gover.backend.payment.models.XBezahldienstePaymentRequest;
+import jakarta.annotation.Nonnull;
 
 import java.math.BigDecimal;
 import java.util.List;
 
-public record FormTriggerCostCalculationResponseV1(BigDecimal totalCost, List<PaymentItem> paymentItems, String paymentProviderName) {
+public record FormTriggerCostCalculationResponseV1(
+        @Nonnull BigDecimal totalCost,
+        @Nonnull List<XBezahldienstePaymentItem> paymentItems,
+        @Nonnull String paymentProviderName
+) {
+    public static FormTriggerCostCalculationResponseV1 empty() {
+        return new FormTriggerCostCalculationResponseV1(
+                BigDecimal.ZERO,
+                List.of(),
+                ""
+        );
+    }
+
+    public static FormTriggerCostCalculationResponseV1 of(XBezahldienstePaymentRequest request, PaymentProviderEntity provider) {
+        return new FormTriggerCostCalculationResponseV1(
+                request.getGrosAmount(),
+                request.getItems(),
+                provider.getName()
+        );
+    }
 }
