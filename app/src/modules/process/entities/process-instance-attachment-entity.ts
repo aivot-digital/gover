e11cdfe46bb0114855dceec42985c6a9f3b4905d
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-08-05 15:20:57 +0200

    refactor: add originalFileName property to attachment interfaces for improved clarity

--- app/src/modules/process/entities/process-instance-attachment-entity.ts
+++ app/src/modules/process/entities/process-instance-attachment-entity.ts
@@ -1,6 +1,7 @@
 export interface ProcessInstanceAttachmentEntity {
     key: string;
     fileName: string;
+    originalFileName: string;
     group?: string | null;
     attachmentSetId: number;
     processInstanceId: number;
