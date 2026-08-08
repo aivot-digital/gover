02702bcdae3804a4ee8f07e9cd92aa232e46fa09
Daniel Kratz <daniel.kratz@aivot.de>
2026-08-08 21:09:58 +0200

    chore: update favicon and theme color in web manifest

--- app/src/components/meta-element/meta-element.tsx
+++ app/src/components/meta-element/meta-element.tsx
@@ -1,5 +1,4 @@
-import React, {useEffect, useMemo} from 'react';
-import {useTheme} from '@mui/material';
+import React, {useMemo} from 'react';
 import {createApiPath} from '../../utils/url-path-utils';
 
 interface MetaElementProps {
@@ -9,15 +8,6 @@ interface MetaElementProps {
 }
 
 export function MetaElement({faviconUrl, title, titlePrefix}: MetaElementProps) {
-    const theme = useTheme();
-
-    useEffect(() => {
-        const root = document.documentElement;
-        root.style.setProperty('--prosuna-theme-primary', theme.palette.primary.main);
-        root.style.setProperty('--prosuna-theme-primary-dark', theme.palette.primary.dark);
-        root.style.setProperty('--prosuna-theme-secondary', theme.palette.secondary.main);
-    }, [theme]);
-
     const favicon = useMemo(() => {
         return faviconUrl ?? createApiPath("/api/public/system/favicon/");
     }, [faviconUrl])
@@ -45,19 +35,10 @@ export function MetaElement({faviconUrl, title, titlePrefix}: MetaElementProps) 
             />
 
             <link
-                key="icon-32x32"
+                key="icon-svg"
                 rel="icon"
-                type="image/png"
-                sizes="32x32"
-                href={`${import.meta.env.BASE_URL}/favicon-32x32.png`}
-            />
-
-            <link
-                key="icon-16x16"
-                rel="icon"
-                type="image/png"
-                sizes="16x16"
-                href={`${import.meta.env.BASE_URL}/favicon-16x16.png`}
+                type="image/svg+xml"
+                href={`${import.meta.env.BASE_URL}/favicon.svg`}
             />
 
             <link
