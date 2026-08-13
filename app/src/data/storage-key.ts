8a1584f0e66f9bb195d3a5f1fb6e886a34b14a40
Daniel Kratz <daniel.kratz@aivot.de>
2026-08-13 02:44:20 +0200

    refactor(frontend): remove obsolete introductory banner

--- app/src/data/storage-key.ts
+++ app/src/data/storage-key.ts
@@ -3,7 +3,6 @@ export enum StorageKey {
     AuthDataRefreshToken = 'refresh_token',
 
     PreReleaseVersionNoticeDismissed = 'pre_release_version_notice_dismissed',
-    IntroDismissed = 'intro_dismissed',
     ColorMode = 'color_mode',
 
     SubmissionsIncludePaymentPending = 'submissions_include_payment_pending',
