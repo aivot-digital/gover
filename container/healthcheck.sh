5a3d0885a2b5f4e2699b1b7fb27868f4ecb61858
Finn Moritz Bätzel <moritz.baetzel@aivot.de>
2026-08-21 10:56:37 +0200

    chore: remove outdated shell entrypoint.sh and healthcheck.sh to replace them with dedicated go tools

--- container/healthcheck.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-
-mode="$(cat /app/runtime-mode 2>/dev/null || echo app)"
-
-if [ "$mode" = "serve" ]; then
-  curl --output /dev/null --silent --fail http://127.0.0.1:${PROSUNA_SERVER_PORT:-8080}/api/actuator/health
-else
-  curl --output /dev/null --silent --fail http://127.0.0.1:8080/
-fi
